f33f1a20dde8775d9b625d52594f6a55c45dde3d
igor02b1 <igor02b1@gmail.com>
2023-02-07 23:59:29 -0300

    feat: turn

--- script.js
+++ script.js
@@ -2,6 +2,18 @@ const celulas = document.querySelectorAll(".quadro");
 let checkturno = true;
 const jogador_X = "X";
 const jogador_O = "O"; 
+const combinacoes = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6]
+];
+
+
 document.addEventListener("click", (event) => {
     if(event.target.matches(".quadro")) {
         jogar(event.target.id);
@@ -12,6 +24,15 @@ function jogar(id){
     const celula = document.getElementById(id);
     turno = checkturno ? jogador_X : jogador_O;
     celula.textContent = turno;
+    celula.classList.add(turno)
     checkturno = !checkturno;
     checarVencedor(turno);
+}
+
+function checkVencedor(turno) {
+    const vencedor = combinacoes.some((comb) => {
+        return comb.every((index) => {
+
+        })
+    });
 }
